712bc75a745f43a73e375eba7e544020a8d8417c
pirren <pierre.nygard@gmail.com>
2023-11-30 00:52:18 +0100

    Runner

---
+++ App.cs
@@ -0,0 +1,91 @@
+﻿using System.Data;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using aoc_runner;
+
+var tSolvers = Assembly.GetEntryAssembly()!.GetTypes()
+    .Where(t => t.GetTypeInfo().IsClass && typeof(ISolver).IsAssignableFrom(t))
+    .OrderBy(t => t.FullName)
+    .ToArray();
+
+var action = Command(args, Args("put", "([0-9]+)/([0-9]+)"), m =>
+    {
+        var year = int.Parse(m[1]);
+        var day = int.Parse(m[2]);
+
+        if(tSolvers.Any(tsolver =>
+            SolverExtensions.Year(tsolver) == year &&
+            SolverExtensions.Day(tsolver) == day))
+        {
+            return () => Console.WriteLine($"Duplicate puzzle. Year = {year}, Day = {day} already exists.");
+        }
+        else if(day is < 1 or > 25)
+        {
+            return () => Console.WriteLine($"Calendar is only 1 through 25.");
+        }
+        return () => Console.WriteLine("TODO: Put new puzzle successfully.");
+    }) ??
+    Command(args, Args("sample","([0-9]+)/(Day)?([0-9]+)"), m =>
+    {
+        var year = int.Parse(m[1]);
+        var day = int.Parse(m[3]);
+
+        var tSolversSelected = tSolvers.First(tsolver =>
+            SolverExtensions.Year(tsolver) == year &&
+            SolverExtensions.Day(tsolver) == day);
+
+        return () => Runner.RunAll(GetSolvers(tSolversSelected), useSampleDatta: true);
+    }) ??
+    Command(args, Args("([0-9]+)/(Day)?([0-9]+)"), m =>
+    {
+        var year = int.Parse(m[0]);
+        var day = int.Parse(m[2]);
+
+        var tSolversSelected = tSolvers.First(tsolver =>
+            SolverExtensions.Year(tsolver) == year &&
+            SolverExtensions.Day(tsolver) == day);
+
+        return () => Runner.RunAll(GetSolvers(tSolversSelected));
+    });
+
+string[] Args(params string[] regex) => regex;
+
+try
+{
+    action();
+}
+catch (AggregateException a)
+{
+    if(a.InnerExceptions.Count == 1 && a.InnerException is AocException)
+        throw a.InnerExceptions[0];
+    else
+        throw;
+}
+
+ISolver[] GetSolvers(params Type[] tsolver) 
+    => tsolver.Select(t => Activator.CreateInstance(t) as ISolver)
+        .Where(x => x != null)
+        .ToArray()!;
+
+Action? Command(string[] args, string[] regexes, Func<string[], Action> parse)
+{
+    if (args.Length != regexes.Length)
+        return null;
+
+    var matches = Enumerable.Zip(args, regexes, (arg, regex) => new Regex("^" + regex + "$").Match(arg));
+    if (!matches.All(match => match.Success))
+        return null;
+
+    try
+    {
+        return parse(matches.SelectMany(m =>
+            m.Groups.Count > 1
+                ? m.Groups.Cast<Group>().Skip(1).Select(g => g.Value)
+                : [m.Value]
+        ).ToArray());
+    }
+    catch
+    {
+        return null;
+    }
+}
---
+++ Lib/Runner.cs
@@ -0,0 +1,127 @@
+﻿using System.Reflection;
+
+namespace aoc_runner;
+
+interface ISolver
+{
+    object PartOne(string input);
+    object PartTwo(string input);
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+class PuzzleName(string name) : Attribute
+{
+    public string Name { get; private set; } = name;
+}
+
+class AocException(string title, string details) : Exception
+{
+    public override string Message => $"{title}\n[{details}]";
+}
+
+static class SolverExtensions
+{
+    public static IEnumerable<object> Solve(this ISolver solver, string input)
+    {
+        yield return solver.PartOne(input);
+        var res = solver.PartTwo(input);
+        if (res != null)
+        {
+            yield return res;
+        }
+    }
+    public static string GetName(this ISolver solver)
+    {
+        try
+        {
+            return (
+                solver
+                    .GetType()
+                    .GetCustomAttribute(typeof(PuzzleName)) as PuzzleName
+            ).Name;
+        } 
+        catch (ArgumentNullException e) 
+        {
+            throw new ArgumentNullException(e.Message);
+        }
+    }
+
+    public static string DayName(this ISolver solver)
+    {
+        return $"Day {solver.Day()}";
+    }
+
+    public static int Year(this ISolver solver)
+    {
+        return Year(solver.GetType());
+    }
+
+    public static int Year(Type t)
+    {
+        return int.Parse(t.FullName.Split('.')[1].Substring(1));
+    }
+
+    public static int Day(this ISolver solver)
+    {
+        return Day(solver.GetType());
+    }
+
+    public static int Day(Type t)
+    {
+        return int.Parse(t.FullName.Split('.')[2][3..]);
+    }
+
+    public static string WorkingDir(int year)
+    {
+        return Path.Combine(year.ToString());
+    }
+
+    public static string WorkingDir(int year, int day)
+    {
+        return Path.Combine(WorkingDir(year), "Day" + day.ToString("00"));
+    }
+
+    public static string WorkingDir(this ISolver solver)
+    {
+        return WorkingDir(solver.Year(), solver.Day());
+    }
+}
+
+class Runner
+{
+    private static string GetNormalizedInput(string file)
+    {
+        var input = File.ReadAllText(file);
+
+        input = input.Replace("\r", "");
+
+        if (input.EndsWith('\n'))
+            input = input[..^1];
+
+        return input;
+    }
+
+    public static void RunSolver(ISolver solver, bool useSampleData)
+    {
+        var workingDir = solver.WorkingDir();
+        var indent = "    ";
+
+        Console.WriteLine($"{indent}{solver.DayName()}: {solver.GetName()}");
+
+        var file = Path.Combine(workingDir, useSampleData ? "sample.in" : "input.in");
+        var input = GetNormalizedInput(file);
+
+        foreach (var result in solver.Solve(input))
+        {
+            Console.WriteLine($"{indent}{result}");
+        }
+    }
+
+    public static void RunAll(ISolver[] solvers, bool useSampleDatta = false)
+    {
+        foreach (var solver in solvers)
+        {
+            RunSolver(solver, useSampleDatta);
+        }
+    }
+}
