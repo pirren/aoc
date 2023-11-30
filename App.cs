46213a717e3099383221fb4614d8b567812f10d7
Pierre Nygård <pierre.nygard@else.se>
2023-11-30 22:13:38 +0100

    Generator

--- App.cs
+++ App.cs
@@ -1,51 +1,67 @@
-﻿using System.Data;
+﻿using aoc_runner;
+using System.Data;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using aoc_runner;
 
 var tSolvers = Assembly.GetEntryAssembly()!.GetTypes()
     .Where(t => t.GetTypeInfo().IsClass && typeof(ISolver).IsAssignableFrom(t))
     .OrderBy(t => t.FullName)
     .ToArray();
 
-var action = Command(args, Args("put", "([0-9]+)/([0-9]+)"), m =>
+var action = Command(args, Args("generate", "([0-9]+)"), m =>
     {
         var year = int.Parse(m[1]);
-        var day = int.Parse(m[2]);
 
         if(tSolvers.Any(tsolver =>
-            SolverExtensions.Year(tsolver) == year &&
-            SolverExtensions.Day(tsolver) == day))
+            SolverExtensions.Year(tsolver) == year))
         {
-            return () => Console.WriteLine($"Duplicate puzzle. Year = {year}, Day = {day} already exists.");
+            return () => Console.WriteLine($"Calendar for year {year} already exists.");
         }
-        else if(day is < 1 or > 25)
-        {
-            return () => Console.WriteLine($"Calendar is only 1 through 25.");
-        }
-        return () => Console.WriteLine("TODO: Put new puzzle successfully.");
+
+        return () => Generator.Generate(year); //Console.WriteLine("TODO: Put new puzzle successfully.");
     }) ??
-    Command(args, Args("sample","([0-9]+)/(Day)?([0-9]+)"), m =>
+    Command(args, Args("sample", "([0-9]+)/(Day)?([0-9]+)"), m =>
     {
+        // Todo: Make sample an optional argument, and combine it with the next command for less code repetition.
         var year = int.Parse(m[1]);
         var day = int.Parse(m[3]);
 
-        var tSolversSelected = tSolvers.First(tsolver =>
+        if (day is < 1 or > 25)
+        {
+            return () => Console.WriteLine($"Calendar is only 1 through 25.");
+        }
+
+        var tSolverSelected = tSolvers.FirstOrDefault(tsolver =>
             SolverExtensions.Year(tsolver) == year &&
             SolverExtensions.Day(tsolver) == day);
 
-        return () => Runner.RunAll(GetSolvers(tSolversSelected), useSampleDatta: true);
+        if (tSolverSelected == null)
+        {
+            return () => Console.WriteLine($"No puzzle found for {year}/{day}.");
+        }
+
+        return () => Runner.RunAll(GetSolvers(tSolverSelected), useSampleDatta: true);
     }) ??
     Command(args, Args("([0-9]+)/(Day)?([0-9]+)"), m =>
     {
         var year = int.Parse(m[0]);
         var day = int.Parse(m[2]);
 
-        var tSolversSelected = tSolvers.First(tsolver =>
+        if (day is < 1 or > 25)
+        {
+            return () => Console.WriteLine($"Calendar is only 1 through 25.");
+        }
+
+        var tSolverSelected = tSolvers.FirstOrDefault(tsolver =>
             SolverExtensions.Year(tsolver) == year &&
             SolverExtensions.Day(tsolver) == day);
 
-        return () => Runner.RunAll(GetSolvers(tSolversSelected));
+        if (tSolverSelected == null)
+        {
+            return () => Console.WriteLine($"No puzzle found for {year}/{day}.");
+        }
+
+        return () => Runner.RunAll(GetSolvers(tSolverSelected));
     });
 
 string[] Args(params string[] regex) => regex;
@@ -54,12 +70,19 @@ try
 {
     action();
 }
-catch (AggregateException a)
+catch (AocException ex)
 {
-    if(a.InnerExceptions.Count == 1 && a.InnerException is AocException)
-        throw a.InnerExceptions[0];
-    else
-        throw;
+    Console.WriteLine(ex.Message);
+}
+catch (NullReferenceException)
+{
+    // Todo: Add a default action.
+    Console.WriteLine("No action.");
+    Environment.Exit(0);
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine(ex.Message);
 }
 
 ISolver[] GetSolvers(params Type[] tsolver) 
@@ -72,7 +95,7 @@ Action? Command(string[] args, string[] regexes, Func<string[], Action> parse)
     if (args.Length != regexes.Length)
         return null;
 
-    var matches = Enumerable.Zip(args, regexes, (arg, regex) => new Regex("^" + regex + "$").Match(arg));
+    var matches = args.Zip(regexes, (arg, regex) => new Regex("^" + regex + "$").Match(arg)).ToArray();
     if (!matches.All(match => match.Success))
         return null;
 
@@ -81,7 +104,7 @@ Action? Command(string[] args, string[] regexes, Func<string[], Action> parse)
         return parse(matches.SelectMany(m =>
             m.Groups.Count > 1
                 ? m.Groups.Cast<Group>().Skip(1).Select(g => g.Value)
-                : [m.Value]
+                : [ m.Value ]
         ).ToArray());
     }
     catch
---
+++ Lib/Generator.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+
+namespace aoc_runner;
+
+static partial class Generator
+{
+    public static Task Generate(int year)
+    {
+        var basePath = "..\\..\\..\\";
+        var yearFolder = $"Y{year}";
+        Directory.CreateDirectory(Path.Combine(basePath, yearFolder));
+        
+        for(var day = 1; day <= 25; day++)
+        {
+            var dayFolder = $"Day{day:00}";
+            var filePath = Path.Combine(basePath, yearFolder, dayFolder, $"Solution.cs");
+            Directory.CreateDirectory(Path.Combine(basePath, yearFolder, dayFolder));
+            GeneratePuzzle(year, day).WriteToFile(filePath);
+        }
+        return Task.CompletedTask;
+    }
+
+    private static void WriteToFile(this string st, string path)
+    {
+        File.WriteAllText(path, st);
+    }
+
+    public static string GeneratePuzzle(int year, int day)
+    {
+        return $@"namespace aoc_runner.Y{year}.Day{day:00};
+             |
+             |[PuzzleName(""Name for puzzle {day}"")]
+             |class Solution : ISolver
+             |{{
+             |    public object PartOne(string input) 
+             |    {{
+             |        return 0;
+             |    }}
+             |
+             |    public object PartTwo(string input) 
+             |    {{
+             |        return 0;
+             |    }}
+             |}}
+             |".StripMargin();
+    }
+
+    public static string StripMargin(this string st, string margin = "|")
+    {
+        return string.Join("\n",
+            from line in MarginPattern().Split(st)
+            select Regex.Replace(line, @"^\s*" + Regex.Escape(margin), "")
+        );
+    }
+
+    [GeneratedRegex("\r?\n")]
+    private static partial Regex MarginPattern();
+}
