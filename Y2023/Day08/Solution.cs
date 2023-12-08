2737448660721f7b868b67884b5f4baeed941bac
pirren <pierre.nygard@gmail.com>
2023-12-08 21:06:22 +0100

    Day8

--- Y2023/Day08/Solution.cs
+++ Y2023/Day08/Solution.cs
@@ -6,81 +6,47 @@ namespace aoc_runner.Y2023.Day08;
 class Solution : ISolver
 {
     public object PartOne(string input) 
-    {
-        return Solve(input);
-    }
+        => Solve(input, (current) => current == "AAA", (current) => current == "ZZZ");
 
     public object PartTwo(string input) 
+        => Solve(input, (current) => current.EndsWith('A'), (current) => current.EndsWith('Z'));
+
+    long Solve(string input, Func<string, bool> starting, Predicate<string> finished)
     {
-        return SolveTwo(input);
+        var directions = Directions(input);
+        var map = Map(input);
+
+        // Each 'track' repeats in length after reaching first end node
+
+        HashSet<long> cycles = [.. map.Keys.Where(starting).Select(start => Traverse(start, directions, map, finished))];
+        return cycles.Aggregate(LCM);
     }
 
-    int Solve(string input)
+    long Traverse(string start, byte[] directions, Dictionary<string, string[]> map, Predicate<string> finished)
     {
-        var (directions, nodes) = Parse(input);
+        var length = 0;
+        var currentLocation = start;
 
-        var steps = 0;
-        var currPos = "AAA";
-        while(currPos != "ZZZ")
+        while (!finished(currentLocation))
         {
-            var direction = directions[steps % directions.Length];
-            steps++;
-            currPos = direction == 'L' ? nodes[currPos][0] : nodes[currPos][1];
+            var direction = directions[length % directions.Length];
+            currentLocation = map[currentLocation][direction];
+            length++;
         }
 
-        return steps;
+        return length;
     }
 
-    int SolveTwo(string input)
-    {
-        var (directions, nodes) = Parse(input);
-        Queue<string> positions = new([.. nodes.Keys.Where(position => position.EndsWith('A'))]);
-        var tracks = positions.Count;
+    long GFC(long a, long b) => b == 0 ? a : GFC(b, a % b);
 
-        for(var steps = 0; steps < int.MaxValue; steps++)
-        {
-            var direction = directions[steps % directions.Length];
-            for (var i = 0; i < positions.Count; i++)
-            {
-                var current = positions.Dequeue();
-                var next = direction == 'L' ? nodes[current][0] : nodes[current][1];
-                if(next.EndsWith('Z')) 
-                    Console.WriteLine("Z at " + steps);
-                positions.Enqueue(next);
-            }
-            if(positions.All(pos => pos.EndsWith('Z'))) return steps + 1;
-        }
+    long LCM(long a, long b) => a / GFC(a, b) * b;
 
-        throw new Exception("Not found");
+    byte[] Directions(string input)
+        => (from m in Regex.Matches(input.Split('\n')[0], @"\w") select (byte)(m.Value[0] == 'L' ? 0 : 1)).ToArray();
 
-        //foreach(var startPosition in nodes.Keys.Where(position => position.EndsWith('A')))
-        //{
-        //    var steps = 0;
-        //    var currPos = startPosition;
-        //    while (!currPos.EndsWith('Z'))
-        //    {
-        //        var direction = directions[steps % directions.Length];
-        //        steps++;
-        //        currPos = direction == 'L' ? nodes[currPos][0] : nodes[currPos][1];
-        //    }
-        //}
-
-    }
-
-    (string directions, Dictionary<string, string[]> map) Parse(string input)
-    {
-        Dictionary<string, string[]> map = [];
-        foreach (var line in input.Split('\n').Skip(2))
-        {
-            var positions = (from m in Regex.Matches(line, @"(\w+)") select m.Value).ToArray();
-            map.Add(positions[0], positions.Skip(1).ToArray());
-        }
-
-        return (input.Split('\n')[0], map);
-    }
-
-    IEnumerable<int> Directions(string input)
-        => from m in Regex.Matches(input, @"\w") select m.Value[0] == 'L' ? 0 : 1;
-
-    record Node(string Self, string Left, string Right);
+    Dictionary<string, string[]> Map(string input)
+        => (from line in input.Split('\n').Skip(2)
+            let positions = (from m in Regex.Matches(line, @"(\w+)") select m.Value).ToArray()
+            select (positions[0], positions.Skip(1).ToArray()))
+        .ToDictionary();
 }
